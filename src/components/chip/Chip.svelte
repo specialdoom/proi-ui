551794ef5426ad499e1d8c926ee0dfa9f058ad64
specialdoom <bledea.bogdan97@gmail.com>
2020-04-14 11:29:43 +0300

    - added chip component

---
+++ src/components/chip/Chip.svelte
@@ -0,0 +1,131 @@
+<script>
+    export let closable = false;
+    export let outlined = false;
+    export let squared = false;
+    export let disabled = false;
+    export let type = "default";
+    export let label = "";
+
+    let closed = false;
+
+    function close() {
+        closed = true;
+    }
+</script>
+<style>
+    .sd-chip {
+        border: none;
+        cursor: default;
+        height: 27px;
+        min-width: 27px;
+        display: inline-flex;
+        outline: 0;
+        color: white;
+        padding: 0;
+        font-size: 0.9rem;
+        box-sizing: border-box;
+        align-items: center;
+        white-space: nowrap;
+        border-radius: 16px;
+        vertical-align: middle;
+        justify-content: center;
+    }
+
+    .sd-chip-squared {
+        border-radius: 4px;
+    }
+
+    .sd-chip-disabled {
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+
+    .sd-chip-outline {
+        background: transparent;
+    }
+
+    .sd-chip-default {
+        background-color: #2398ab;
+    }
+
+    .sd-chip-default-outline {
+        border: 1px solid #2398ab;
+        color: #2398ab;
+    }
+
+    .sd-chip-primary {
+        background-color: #79aaf3;
+    }
+
+    .sd-chip-primary-outline {
+        border: 1px solid #79aaf3;
+        color: #79aaf3;
+    }
+
+    .sd-chip-secondary {
+        background-color: #ff6a61;
+    }
+
+    .sd-chip-secondary-outline {
+        border: 1px solid #ff6a61;
+        color: #ff6a61;
+    }
+
+    .sd-chip-label {
+        overflow: hidden;
+        white-space: nowrap;
+        padding-left: 12px;
+        padding-right: 12px;
+        text-overflow: ellipsis;
+    }
+
+    .sd-chip-close-icon {
+        display: flex;
+        align-items: center;
+        margin-left: auto;
+        padding-left: 16px;
+        opacity: 0.5;
+    }
+
+    svg {
+        padding-right: 5px;
+        padding-top: 2px;
+        height: 13px;
+        fill: white;
+        opacity: 0.7;
+    }
+
+    .sd-svg-default {
+        fill: #2398ab;
+    }
+
+    .sd-svg-primary {
+        fill: #79aaf3;
+    }
+
+    .sd-svg-secondary {
+        fill: #ff6a61;
+    }
+
+    svg:hover {
+        opacity: 1;
+    }
+
+
+</style>
+{#if !closed}
+    <div on:click class="sd-chip {disabled ? 'sd-chip-disabled' : '' } {squared ? 'sd-chip-squared' : ''} {outlined ? `sd-chip-outline sd-chip-${type}-outline` : `sd-chip-${type}`}">
+    <span class="sd-chip-label">
+        {label}
+    </span>
+        {#if closable && disabled === false}
+            <svg id="icon" data-name="error" viewBox="0 0 12 12" class="{outlined ? `sd-svg-${type}`: ''}"
+                 on:click={close}><title>
+                icon</title>
+                <g id="icon-content" data-name="content">
+                    <path d="M6,0a6,6,0,1,0,6,6A6,6,0,0,0,6,0ZM8.51,7.9a.43.43,0,0,1-.61.61L6,6.61,4.1,8.51a.43.43,0,0,1-.61-.61L5.39,6,3.49,4.1a.43.43,0,0,1,.61-.61L6,5.39l1.9-1.9a.43.43,0,0,1,.61.61L6.61,6Z"></path>
+                </g>
+            </svg>
+        {/if}
+    </div>
+{/if}
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export {default as Checkbox} from './components/checkbox/Checkbox.svelte';
 export {default as Radio} from './components/radio/Radio.svelte';
 export {default as Dropdown} from './components/dropdown/Dropdown.svelte';
 export {default as Alert} from './components/alert/Alert.svelte';
+export {default as Chip} from './components/chip/Chip.svelte';
