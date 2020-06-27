d7b11a723687fa47fb9b46b68e447c5be234db26
specialdoom <bledea.bogdan97@gmail.com>
2020-06-27 14:53:04 +0300

    recover spinner component

---
+++ src/components/spinner/Spinner.svelte
@@ -0,0 +1,78 @@
+<script>
+    export let labeled = false;
+    export let label = "Loading...";
+    export let small = false;
+
+</script>
+
+<style>
+    .sd-spinner-container {
+        display: flex;
+        max-width: 150px;
+        word-wrap: break-word;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-weight: 100;
+        font-size: 12px;
+    }
+
+    .sd-spinner-small {
+        height: 30px;
+        width: 30px;
+    }
+
+    .sd-spinner {
+        height: 60px;
+        width: 60px;
+    }
+</style>
+
+<span class="sd-spinner-container">
+    <svg version="1.1" id="spinner" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+         x="0px"
+         y="0px"
+         viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve"
+         class="{small ? 'sd-spinner-small' : 'sd-spinner'}">
+<style type="text/css">
+	.st0 {
+        fill: none;
+        stroke: #2398AB;
+        stroke-width: 3;
+        stroke-linecap: round;
+        stroke-miterlimit: 10;
+    }
+</style>
+        <g>
+	<path class="st0" d="M50,95"/>
+            <path class="st0" d="M50,95C25.1,95,5,74.9,5,50S25.1,5,50,5s45,20.1,45,45	">
+<animateTransform accumulate="none" additive="replace" attributeName="transform" attributeType="XML" calcMode="linear"
+                  dur="3s" fill="remove" from="0 50 50" repeatCount="indefinite" restart="always" to="360 50 50"
+                  type="rotate">
+			</animateTransform>
+	</path>
+</g>
+        <g>
+	<path class="st0" d="M50,21"/>
+            <path class="st0" d="M50,21c16,0,29,13,29,29S66,79,50,79S21,66,21,50	">
+<animateTransform accumulate="none" additive="replace" attributeName="transform" attributeType="XML" calcMode="linear"
+                  dur="3s" fill="remove" from="360 50 50" repeatCount="indefinite" restart="always" to="0 50 50"
+                  type="rotate">
+			</animateTransform>
+	</path>
+</g>
+        <g>
+	<path class="st0" d="M50,62"/>
+            <path class="st0" d="M50,62c-6.6,0-12-5.4-12-12s5.4-12,12-12s12,5.4,12,12	">
+<animateTransform accumulate="none" additive="replace" attributeName="transform" attributeType="XML" calcMode="linear"
+                  dur="3s" fill="remove" from="0 50 50" repeatCount="indefinite" restart="always" to="360 50 50"
+                  type="rotate">
+			</animateTransform>
+	</path>
+</g>
+</svg>
+    {#if labeled && !small}
+        {label}
+    {/if}
+</span>
--- src/index.js
+++ src/index.js
@@ -8,4 +8,5 @@ export { default as Chip } from './components/chip/Chip.svelte';
 export { default as Backdrop } from './components/backdrop/Backdrop.svelte';
 export { default as Modal } from './components/modal/Modal.svelte';
 export { default as Progress } from './components/progress/Progress.svelte';
+export { default as Spinner } from './components/spinner/Spinner.svelte';
 export { default as Avatar } from './components/avatar/Avatar.svelte';
