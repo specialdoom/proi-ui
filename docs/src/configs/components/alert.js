85dc567660c96c73d0ff8b006998a45dda6d317f
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-06-20 13:08:28 +0300

    Refactor avatar component

--- docs/src/configs/components/alert.js
+++ docs/src/configs/components/alert.js
@@ -1,9 +1,4 @@
-export const columns = [
-  { key: 1, value: 'Property' },
-  { key: 2, value: 'Description' },
-  { key: 3, value: 'Type' },
-  { key: 4, value: 'Default' }
-];
+import { columns } from '../shared/columns';
 
 export const data = [
   {
---
+++ docs/src/configs/components/avatar.js
@@ -0,0 +1,35 @@
+import { columns } from '../shared/columns';
+
+export const data = [
+  {
+    property: 'title',
+    description: 'Title of the avatar',
+    type: 'string',
+    default: `''`
+  },
+  {
+    property: 'description',
+    description: 'Short description of the avatar',
+    type: 'string',
+    default: `''`
+  },
+  {
+    property: 'image',
+    description: 'Source of avatar image',
+    type: 'string',
+    default: `''`
+  }
+];
+
+export const usage = `<script>
+ import {Avatar} from 'proi-ui';
+</script>;
+
+<Avatar title="Proi Ui" description="Library" />
+<Avatar
+  title="specialdoom"
+  description="creator"
+  image="https://avatars.githubusercontent.com/u/41910815?v=4"
+/>`;
+
+export const config = { title: 'Avatar component', columns, data, usage };
