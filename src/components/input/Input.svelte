a12bf32730e4b426da619397f91ca07ef7db3819
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-06-21 09:13:18 +0300

    Refactor input component

--- src/components/input/Input.svelte
+++ src/components/input/Input.svelte
@@ -1,31 +1,42 @@
 <script>
   import { onMount } from 'svelte';
-
-  let element;
+  import { inputTypes } from './types';
 
   export let type = 'text';
   export let placeholder = '';
-  export let value = null;
-  export let squared = false;
+  export let value = '';
+  export let label = '';
   export let error = '';
-  export let success = false;
-  export let label = 'Label';
   export let disabled = false;
 
+  let element;
+
   onMount(() => {
-    if (type !== 'text' && type !== 'password') {
-      type = 'text';
-    }
+    if (!inputTypes.includes(type)) type = 'text';
     element.type = type;
   });
 </script>
 
+<div class="sd-input-container">
+  <span class="sd-label" class:sd-label-error={error}>{label}</span>
+  <input
+    {placeholder}
+    bind:value
+    class="sd-input"
+    class:sd-input-error={error}
+    on:change
+    bind:this={element}
+    {disabled}
+  />
+  <div class="sd-error">{error}</div>
+</div>
+
 <style>
   .sd-input {
     border: 1px solid grey;
-    border-radius: 50px;
+    border-radius: 4px;
     width: 100%;
-    height: 33px !important;
+    height: 30px;
     font-size: 13px;
     margin: 0;
     padding: 0 0 0 15px;
@@ -39,10 +50,6 @@
     cursor: not-allowed;
   }
 
-  .sd-input-squared {
-    border-radius: 5px;
-  }
-
   .sd-input-error {
     border-color: #ff6a61;
     color: #ff6a61;
@@ -54,11 +61,6 @@
     font-size: 12px;
     line-height: 15px;
     margin: 7px 0 0;
-  }
-
-  .sd-input-success {
-    border-color: #35d073;
-    color: #35d073;
   }
 
   .sd-label {
@@ -76,18 +78,3 @@
     transition: all ease 0.5s;
   }
 </style>
-
-<div class="sd-input-container">
-  <span
-    class="sd-label {error ? 'sd-label-error' : ''}">{label}</span>
-  <input
-    {placeholder}
-    bind:value
-    class="sd-input {error ? 'sd-input-error' : ''}
-    {success ? 'sd-input-success' : ''}
-    {squared ? 'sd-input-squared' : ''}"
-    on:change
-    bind:this={element}
-    {disabled} />
-  <div class="sd-error">{error}</div>
-</div>
