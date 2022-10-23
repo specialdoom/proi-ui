3f0f1cb3ab8bd6f152f8ecb3eececd94bd893564
specialdoom <bledea.bogdan97@gmail.com>
2022-10-23 11:47:08 +0300

    Finish histoire integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { HstSvelte } from '@histoire/plugin-svelte'
 
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	histoire: {
-		plugins: [
-			HstSvelte(),
-		],
-		setupFile: './histoire.setup.js',
-		storyIgnored: ['**/node_modules/**', '**/package/**']
-	}
 };
 
 export default config;
