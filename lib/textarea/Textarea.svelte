f1662a60d6300bc6293cc35caacfe708ce7c496a
BB <41910815+specialdoom@users.noreply.github.com>
2022-11-26 18:41:33 +0200

    feat: add `Textarea` component (#72)
    * chore: add Textarea component
    
    * chore: update prettierrc.cjs

---
+++ lib/textarea/Textarea.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  export let placeholder: string = "";
+  export let value: string = "";
+  export let error: boolean = false;
+  export let disabled: boolean = false;
+  export let rows: number = 4;
+</script>
+
+<textarea
+  {placeholder}
+  bind:value
+  class="proi-textarea"
+  class:error
+  {rows}
+  {disabled}
+  on:change
+  on:keydown
+/>
+
+<style>
+  .proi-textarea {
+    border: 2px solid var(--n200);
+    border-radius: 4px;
+    width: 100%;
+    font-size: 14px;
+    padding: 6px 12px;
+    box-sizing: border-box;
+    line-height: 20px;
+    outline: none;
+    color: var(--n800);
+  }
+
+  .proi-textarea::placeholder {
+    color: var(--n400);
+  }
+
+  .proi-textarea:disabled {
+    background: var(--n200);
+    color: var(--n800);
+    outline: none;
+  }
+
+  .proi-textarea:disabled:focus,
+  .proi-textarea:disabled:active {
+    outline: none;
+  }
+
+  .proi-textarea:focus,
+  .proi-textarea:active {
+    outline: 2px solid var(--g200);
+  }
+
+  .proi-textarea.error {
+    border: 2px solid var(--r200);
+    background: var(--bright);
+    color: var(--r600);
+    outline: none;
+  }
+
+  .proi-textarea.error::placeholder {
+    color: var(--r200);
+  }
+</style>
