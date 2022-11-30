43bdc797e568ffb0108f709b68eae07adcbcaed4
specialdoom <bledea.bogdan97@gmail.com>
2022-11-30 12:57:58 +0200

    chore: add Button component tests

--- lib/button/Button.svelte
+++ lib/button/Button.svelte
@@ -4,15 +4,13 @@
   export let variant: ButtonVariant = "primary";
   export let disabled: boolean = false;
   export let block: boolean = false;
-  export let className: string = "";
 </script>
 
 <button
-  class="proi-button {variant} {className}"
+  class="proi-button {variant}"
   class:block
   {disabled}
   on:click
-  data-testid="proi-button"
 >
   <slot />
 </button>
