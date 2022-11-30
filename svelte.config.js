42e46f6ef2ecab993034409d3a6135bba96b4d61
specialdoom <bledea.bogdan97@gmail.com>
2022-11-30 17:40:28 +0200

    chore: update svelte config and CHANGELOG.md

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		files: (filepath) => {
 			if (filepath.includes("/__stories__/")) return false;
 			if (filepath.endsWith(".story.svelte")) return false;
-			if (filepath.includes("/__tests__/")) return false;
+			if (filepath.includes("__tests__")) return false;
 
 			return true;
 		}
