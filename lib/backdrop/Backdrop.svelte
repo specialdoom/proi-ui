f220cd5f5191266bed3e25c64a862dcccf43407c
specialdoom <bledea.bogdan97@gmail.com>
2022-12-02 14:03:30 +0200

    chore: move Backdrop stories to __stories__ folder

--- lib/backdrop/Backdrop.svelte
+++ lib/backdrop/Backdrop.svelte
@@ -21,11 +21,11 @@
     left: 0;
     display: flex;
     position: fixed;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     align-items: center;
     justify-content: center;
     background-color: rgba(0, 0, 0, 0.5);
-    z-index: 1200;
+    z-index: 9000;
   }
 </style>
--- lib/backdrop/__stories__/Backdrop.story.svelte
+++ lib/backdrop/__stories__/Backdrop.story.svelte
@@ -13,40 +13,25 @@
     title="Default"
     source={defaultSource}
   >
-    <div
-      style:height="100vh"
-      style:width="100%"
-    >
-      <Backdrop visible>Backdrop content</Backdrop>
-    </div>
+    <Backdrop visible>Backdrop content</Backdrop>
   </Hst.Variant>
 
   <Hst.Variant
     title="With custom content"
     source={withCustomContent}
   >
-    <div
-      style:height="100vh"
-      style:width="100%"
-    >
-      <Backdrop visible>
-        <Badge>Custom content</Badge>
-      </Backdrop>
-    </div>
+    <Backdrop visible>
+      <Badge>Custom content</Badge>
+    </Backdrop>
   </Hst.Variant>
 
   <Hst.Variant
     title="on:click"
     source={onClickSource}
   >
-    <div
-      style:height="100vh"
-      style:width="100%"
+    <Backdrop
+      visible
+      on:click={(event) => logEvent("Click", event)}>Backdrop content</Backdrop
     >
-      <Backdrop
-        visible
-        on:click={(event) => logEvent("Click", event)}>Backdrop content</Backdrop
-      >
-    </div>
   </Hst.Variant>
 </Hst.Story>
