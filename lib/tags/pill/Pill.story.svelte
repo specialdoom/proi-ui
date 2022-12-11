006c8cc3d21e444b4e0360b03f63d4db255e1123
specialdoom <bledea.bogdan97@gmail.com>
2022-12-11 12:31:33 +0200

    feat: add `bright` variant, update `neutral` variant and, new `label` property for `Pill` component

---
+++ lib/tags/pill/Pill.story.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Hst as HstType } from "@histoire/plugin-svelte";
+  import { capitalizeFirstLetter, dataDisplayVariants } from "../../__stories__/utils.js";
+  import Pill from "./Pill.svelte";
+
+  export let Hst: HstType;
+</script>
+
+<Hst.Story title="Tags/Pill">
+  <Hst.Variant title="Default">
+    <Pill />
+  </Hst.Variant>
+
+  <Hst.Variant title="With content">
+    <Pill label="Content" />
+  </Hst.Variant>
+
+  {#each dataDisplayVariants as variant}
+    {@const title = capitalizeFirstLetter(variant)}
+    <Hst.Variant title="{title} Pill">
+      <Pill
+        {variant}
+        label={title}
+      />
+    </Hst.Variant>
+  {/each}
+</Hst.Story>
