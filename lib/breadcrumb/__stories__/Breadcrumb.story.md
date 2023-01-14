95bc55fc2780098afee7d9482c06cb71f504766f
specialdoom <bledea.bogdan97@gmail.com>
2023-01-14 13:18:58 +0200

    docs: add `Breadcrumb` story

---
+++ lib/breadcrumb/__stories__/Breadcrumb.story.md
@@ -0,0 +1,9 @@
+### Properties
+
+#### BreadcrumbItem
+
+| Property    | Description                                         | Type      | Default     |
+| ----------- | --------------------------------------------------- | --------- | ----------- |
+| `href`      | Set the `href` attribute for anchor tag. (required) | `string`  | `undefined` |
+| `text`      | Inner text of anchor tag element. (required)        | `string`  | `undefined` |
+| `isCurrent` | Whether the anchor tag represents current page .    | `boolean` | `false`     |
---
+++ lib/breadcrumb/__stories__/Breadcrumb.story.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import type { Hst as HstType } from "@histoire/plugin-svelte";
+  import Breadcrumb from "../Breadcrumb.svelte";
+  import BreadcrumbItem from "../BreadcrumbItem.svelte";
+
+  export let Hst: HstType;
+</script>
+
+<Hst.Story title="Breadcrumb">
+  <Hst.Variant title="Default">
+    <Breadcrumb>
+      <BreadcrumbItem
+        href="#item1"
+        text="Item1"
+      />
+      <BreadcrumbItem
+        href="#item2"
+        text="Item2"
+      />
+      <BreadcrumbItem
+        href="#item3"
+        text="Item3"
+        isCurrent
+      />
+    </Breadcrumb>
+
+    <div
+      id="item1"
+      style:height="300px"
+    >
+      Item 1
+    </div>
+    <div
+      id="item2"
+      style:height="300px"
+    >
+      Item 2
+    </div>
+    <div
+      id="item3"
+      style:height="300px"
+    >
+      Item 3
+    </div>
+  </Hst.Variant>
+</Hst.Story>
