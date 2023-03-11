cbe9d1b6831798ce08504f4da2a9b548a73249f3
specialdoom <bledea.bogdan97@gmail.com>
2023-03-11 14:10:38 +0200

    chore: update `vitest` config to use `reporter`

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: "jsdom",
-		root: "lib"
+		reporters: process.env.CI ? "junit" : "default",
+		outputFile: "test-results.xml",
 	}
 })
