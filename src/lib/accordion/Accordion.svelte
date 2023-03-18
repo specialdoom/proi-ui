73bc152a8f7fc6495c618dfa8544db76bf755708
Bledea Bogdan <41910815+specialdoom@users.noreply.github.com>
2023-03-18 17:27:36 +0000

    feat: add `Accordion` component (#96)
    * chore: add accordion components
    
    * chore: update `AccordionItem` component
    
    * test: add `AccordionItem` unit tests
    
    * test: add `Accordion` component unit tests
    
    * docs: update `Accordion` stories
    
    * chore: add component for export

---
+++ src/lib/accordion/Accordion.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<ul
+  class:proi-accordion={true}
+  {...$$restProps}
+  on:click
+  on:focus
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+>
+  <slot />
+</ul>
+
+<style>
+  .proi-accordion {
+    width: 100%;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/lib/accordion/__tests__/accordion-item.selectors.ts
@@ -0,0 +1,5 @@
+/** Component's element(s) selector(s) */
+export const ROOT = ":scope > div > li";
+export const ACCORDION_ITEM_CONTROL = `${ROOT} > .proi-accordion-item-control`;
+export const ACCORDION_ITEM_CONTROL_ICON = `${ACCORDION_ITEM_CONTROL} > svg`;
+export const ACCORDION_ITEM_CONTENT = `${ROOT} > .proi-accordion-item-content`;
