06f33a3028bdd3be417a4dcae574ce0bcd855e91
specialdoom <bledea.bogdan97@gmail.com>
2023-05-01 14:14:01 +0300

    chore: add unit tests for `FillCard` component

--- src/lib/card/__tests__/fill-card.selectors.ts
+++ src/lib/card/__tests__/fill-card.selectors.ts
@@ -1,4 +1,5 @@
 /** Component's element(s) selector(s) */
 export const ROOT = ":scope > div > div";
 export const TITLE = `${ROOT} > h2`;
+export const CLOSE_BUTTON = `${TITLE} > button`;
 export const DESCRIPTION = `${ROOT} > .proi-card-description`;
--- src/lib/card/__tests__/fill-card.test.ts
+++ src/lib/card/__tests__/fill-card.test.ts
@@ -1,132 +1,187 @@
-import { describe } from "vitest";
+import { describe, vi } from "vitest";
 import { render } from "../../../__tests__/tests.queries.js";
-import { TITLE, DESCRIPTION, ROOT } from "./fill-card.selectors.js";
+import { TITLE, DESCRIPTION, ROOT, CLOSE_BUTTON } from "./fill-card.selectors.js";
 import FillCard from "../FillCard.svelte";
 import { dataDisplayVariants } from "../../utils/variants.js";
+import { fireEvent } from "@testing-library/svelte";
 
 describe("FillCard", () => {
-  describe("Card", () => {
-    const title = "Title";
-    const description = "Description";
+  const title = "Title";
+  const description = "Description";
 
-    describe("root element", () => {
-      it("should be in the document", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(ROOT)).toBeInTheDocument();
+  describe("root element", () => {
+    it("should be in the document", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
       });
 
-      it("should contain `proi-card` class", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(ROOT)).toHaveClass("proi-card");
-      });
-
-      it("should contain `data-display` class", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(ROOT)).toHaveClass("data-display");
-      });
+      expect(getBySelector(ROOT)).toBeInTheDocument();
     });
 
-    describe("title element", () => {
-      it("should be in the document", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(TITLE)).toBeInTheDocument();
+    it("should contain `proi-card` class", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
       });
 
-      it("should contain `proi-card-title` class", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(TITLE)).toHaveClass("proi-card-title");
-      });
-
-      it("should contain `title` property as innerText", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(TITLE)).toHaveTextContent(title);
-      });
+      expect(getBySelector(ROOT)).toHaveClass("proi-card");
     });
 
-    describe("description element", () => {
-      it("should be in the document", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(DESCRIPTION)).toBeInTheDocument();
+    it("should contain `data-display` class", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
       });
 
-      it("should contain `proi-card-description` class", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
+      expect(getBySelector(ROOT)).toHaveClass("data-display");
+    });
+  });
 
-        expect(getBySelector(DESCRIPTION)).toHaveClass("proi-card-description");
+  describe("title element", () => {
+    it("should be in the document", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
       });
 
-      it("should contain `description` property as innerText", () => {
-        const { getBySelector } = render(FillCard, {
-          props: {
-            title,
-            description
-          }
-        });
-
-        expect(getBySelector(DESCRIPTION)).toHaveTextContent(description);
-      });
+      expect(getBySelector(TITLE)).toBeInTheDocument();
     });
 
-    describe("when `variant` property", () => {
-      dataDisplayVariants.forEach((variant) => {
-        describe("root element", () => {
-          it(`should contain \`${variant}\` class`, () => {
-            const { getBySelector } = render(FillCard, {
+    it("should contain `proi-card-title` class", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
+      });
+
+      expect(getBySelector(TITLE)).toHaveClass("proi-card-title");
+    });
+
+    it("should contain `title` property as innerText", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
+      });
+
+      expect(getBySelector(TITLE)).toHaveTextContent(title);
+    });
+  });
+
+  describe("description element", () => {
+    it("should be in the document", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
+      });
+
+      expect(getBySelector(DESCRIPTION)).toBeInTheDocument();
+    });
+
+    it("should contain `proi-card-description` class", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
+      });
+
+      expect(getBySelector(DESCRIPTION)).toHaveClass("proi-card-description");
+    });
+
+    it("should contain `description` property as innerText", () => {
+      const { getBySelector } = render(FillCard, {
+        props: {
+          title,
+          description
+        }
+      });
+
+      expect(getBySelector(DESCRIPTION)).toHaveTextContent(description);
+    });
+  });
+
+  describe("when `variant` property", () => {
+    dataDisplayVariants.forEach((variant) => {
+      describe("root element", () => {
+        it(`should contain \`${variant}\` class`, () => {
+          const { getBySelector } = render(FillCard, {
+            props: {
+              title,
+              description,
+              variant
+            }
+          });
+
+          expect(getBySelector(ROOT)).toHaveClass(variant);
+        });
+      });
+    });
+  });
+
+  describe("when `closable` property", () => {
+    describe("is set to `true`", () => {
+      const closable = true;
+
+      describe("close button element", () => {
+        it("should be in the document", () => {
+          const { getBySelector } = render(FillCard, {
+            props: {
+              title,
+              description,
+              closable
+            }
+          });
+
+          expect(getBySelector(CLOSE_BUTTON)).toBeInTheDocument();
+        });
+
+        describe("when `click` event is triggered", () => {
+          it("should dispatch `close` event", async () => {
+            const mock = vi.fn();
+
+            const { component, getBySelector } = render(FillCard, {
               props: {
                 title,
                 description,
-                variant
+                closable
               }
             });
 
-            expect(getBySelector(ROOT)).toHaveClass(variant);
+            component.$on("close", mock);
+
+            await fireEvent.click(getBySelector(CLOSE_BUTTON));
+
+            expect(mock).toHaveBeenCalled();
+          });
+
+          describe("root element", () => {
+            it("should not be in the document", async () => {
+              const { getBySelector } = render(FillCard, {
+                props: {
+                  title,
+                  description,
+                  closable
+                }
+              });
+
+              await fireEvent.click(getBySelector(CLOSE_BUTTON));
+
+              expect(() => getBySelector(ROOT)).toThrowError();
+            });
           });
         });
       });
