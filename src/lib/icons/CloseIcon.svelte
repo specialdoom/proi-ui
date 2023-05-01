3bb01dd555903b0dd22e852efd18c44308ad04e3
specialdoom <bledea.bogdan97@gmail.com>
2023-05-01 10:34:05 +0300

    chore: update icons

--- src/lib/icons/CloseIcon.svelte
+++ src/lib/icons/CloseIcon.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  export let stroke: string = "#223843";
+</script>
+
 <svg
   width="20"
   height="20"
@@ -7,14 +11,14 @@
 >
   <path
     d="M15 5L5 15"
-    stroke="#EFF1F3"
+    {stroke}
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
   />
   <path
     d="M5 5L15 15"
-    stroke="#EFF1F3"
+    {stroke}
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
